5d5d353ee73819ff7f6e6ecb1f788450756048c3
nicolaskbs <83240524+nicolaskbs@users.noreply.github.com>
2025-07-22 08:01:35 -0300

    Sobrecarga da igualdade

--- libs/user.hpp
+++ libs/user.hpp
@@ -17,4 +17,9 @@ struct User{
         userid(userid), 
         region(region), 
         password(password) {};
+
+    // Sobrecarga do operador de igualdade para facilitar a busca
+    bool operator==(const User& other) const {
+        return userid == other.userid;
+    }
 };
